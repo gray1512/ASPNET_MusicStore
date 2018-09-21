1c07a1ec0f043a610e0f7152367c87b7b2f7ba23
Gray Pham <graypham98@gmail.com>
2018-09-21 13:34:25 -0400

    Week 3 upgraded

---
+++ MusicStore/Controllers/AlbumsController.cs
@@ -0,0 +1,27 @@
+﻿using MusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicStore.Controllers
+{
+    public class AlbumsController : Controller
+    {
+        // GET: Albums
+        public ActionResult Index()
+        {
+            // Create a mock list of type Album
+            var albums = new List<Album>();
+
+            for(int i = 1; i <= 10; i++)
+            {
+                albums.Add(new Album { Title = "Album " + i.ToString() });
+            }
+            // ViewBag.albums = albums.OrderBy(a => a.Title);
+            
+            return View(albums);
+        }
+    }
+}
