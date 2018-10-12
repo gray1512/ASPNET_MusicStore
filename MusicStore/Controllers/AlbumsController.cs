4031934540b91cdee2e9acfe1afd4ebd23d41b8a
Gray Pham <graypham98@gmail.com>
2018-10-12 13:36:53 -0400

    Add SSL

--- MusicStore/Controllers/AlbumsController.cs
+++ MusicStore/Controllers/AlbumsController.cs
@@ -1,27 +1,151 @@
-﻿using MusicStore.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using MusicStore.Models;
 
 namespace MusicStore.Controllers
 {
+    [Authorize(Roles = "Administrator")]
     public class AlbumsController : Controller
     {
+        private MusicStoreModels db = new MusicStoreModels();
+
         // GET: Albums
         public ActionResult Index()
         {
-            // Create a mock list of type Album
-            var albums = new List<Album>();
+            var album = db.Album.Include(a => a.Artist).Include(a => a.Genre)
+                .OrderBy(a => a.Artist.Name).ThenBy(a => a.Title);
+            return View(album.ToList());
+        }
 
-            for(int i = 1; i <= 10; i++)
+        // [OverrideAuthorization]
+        [AllowAnonymous]
+        // GET: Albums/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
             {
-                albums.Add(new Album { Title = "Album " + i.ToString() });
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            // ViewBag.albums = albums.OrderBy(a => a.Title);
-            
-            return View(albums);
+            Album album = db.Album.Find(id);
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            return View(album);
+        }
+
+        // GET: Albums/Create
+        public ActionResult Create()
+        {
+            ViewBag.ArtistId = new SelectList(db.Artist, "ArtistId", "Name");
+            ViewBag.GenreId = new SelectList(db.Genre, "GenreId", "Name");
+            return View();
+        }
+
+        // POST: Albums/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "AlbumId,GenreId,ArtistId,Title,Price,AlbumArtUrl")] Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Request.Files.Count > 0)
+                {
+                    var file = Request.Files[0];
+                    if (file.FileName != null && file.ContentLength > 0)
+                    {
+                        // Get file path dynamically
+                        string path = Server.MapPath("~/Content/Images/") + file.FileName;
+                        file.SaveAs(path);
+                        album.AlbumArtUrl = "/Content/Images/" + file.FileName;
+                    }
+                }
+                db.Album.Add(album);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ArtistId = new SelectList(db.Artist, "ArtistId", "Name", album.ArtistId);
+            ViewBag.GenreId = new SelectList(db.Genre, "GenreId", "Name", album.GenreId);
+            return View(album);
+        }
+
+        // GET: Albums/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Album album = db.Album.Find(id);
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ArtistId = new SelectList(db.Artist, "ArtistId", "Name", album.ArtistId);
+            ViewBag.GenreId = new SelectList(db.Genre, "GenreId", "Name", album.GenreId);
+            return View(album);
+        }
+
+        // POST: Albums/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "AlbumId,GenreId,ArtistId,Title,Price,AlbumArtUrl")] Album album)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(album).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ArtistId = new SelectList(db.Artist, "ArtistId", "Name", album.ArtistId);
+            ViewBag.GenreId = new SelectList(db.Genre, "GenreId", "Name", album.GenreId);
+            return View(album);
+        }
+
+        // GET: Albums/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Album album = db.Album.Find(id);
+            if (album == null)
+            {
+                return HttpNotFound();
+            }
+            return View(album);
+        }
+
+        // POST: Albums/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Album album = db.Album.Find(id);
+            db.Album.Remove(album);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
--- MusicStore/Models/Album.cs
+++ MusicStore/Models/Album.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
 namespace MusicStore.Models
 {
-    public class Album
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Album")]
+    public partial class Album
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Album()
+        {
+            Cart = new HashSet<Cart>();
+            OrderDetail = new HashSet<OrderDetail>();
+        }
+
+        public int AlbumId { get; set; }
+
+        public int GenreId { get; set; }
+
+        public int ArtistId { get; set; }
+
+        [Required]
+        [StringLength(160)]
+        [Display(Name = "Album Title")]
         public string Title { get; set; }
+
+        [Column(TypeName = "numeric")]
+        [DataType(DataType.Currency)]
+        [Range(0, 100000)]
+        public decimal Price { get; set; }
+
+        [StringLength(1024)]
+        [Display(Name = "Cover")]
+        public string AlbumArtUrl { get; set; }
+
+        public virtual Artist Artist { get; set; }
+
+        public virtual Genre Genre { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Cart> Cart { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<OrderDetail> OrderDetail { get; set; }
     }
 }
